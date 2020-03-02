8a83c65dbd24184cc3481a72a6ecf74d0a825d34
Azhang9328 <azhang9328@gmail.com>
2020-03-02 13:42:17 -0800

    search request returns temp obj

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,11 @@
 class EmailsController < ApplicationController
     def search 
+        email = Email.new(address: params["search"])
+        email.search
+        render json: email
+    end
+
+    def save 
         
     end
 end
