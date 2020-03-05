78ebc675a86131fc925c8302a9afbbde36daad39
Azhang9328 <azhang9328@gmail.com>
2020-03-04 16:11:02 -0800

    adjusted serializer

--- app/serializers/breach_serializer.rb
+++ app/serializers/breach_serializer.rb
@@ -1,5 +1,4 @@
 class BreachSerializer < ActiveModel::Serializer
-  attributes :id, :name, :title, :domain, :breach_date, :pwn_count, :description, :data_classes, :is_verified, :is_fabricated, :is_retired, :is_spam_list, :logo_path
-  has_many :emails_breaches
-  has_many :emails, through: :emails_breaches
+  attributes :id, :name, :title, :domain, :breach_date, :pwn_count, :description, :data_classes, :is_spam_list, :logo_path
+  has_many :emails
 end
--- app/serializers/email_serializer.rb
+++ app/serializers/email_serializer.rb
@@ -1,5 +1,4 @@
 class EmailSerializer < ActiveModel::Serializer
   attributes :id, :address
-  has_many :emails_breaches
-  has_many :breaches, through: :emails_breaches
+  has_many :breaches
 end
