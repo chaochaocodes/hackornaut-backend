0e18545f5834aae71791a17fc88fe31bdbe52e88
github username <github email address>
2020-03-04 16:15:34 -0800

    added path for breaches,

--- app/controllers/breaches_controller.rb
+++ app/controllers/breaches_controller.rb
@@ -3,4 +3,9 @@ class BreachesController < ApplicationController
         @breaches = Breach.all
         render json: @breaches
     end
+
+    def thirty
+        @thirtyBreaches = Breach.first(30)
+        render json: @thirtyBreaches
+    end
 end
